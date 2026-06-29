25938debb6971514e58c364d7bdf903594446656
demserv <demserv@gmail.com>
2026-06-29 20:24:13 -0300

    T-11: EVENT BUS - normalizar nomenclatura de eventos
    - event_bus.h: #define EVENT_ID_* -> enum event_id_t (type-safe)
    - +EVENT_ID_COUNT, EVENT_BUS_MAX_SUBSCRIBERS, EVENT_BUS_MAX_PENDING
    - event_bus.c: MAX_SUBSCRIBERS_PER_EVENT/MAX_PENDING_EVENTS -> header defines

--- firmware/include/event_bus.h
+++ firmware/include/event_bus.h
@@ -11,28 +11,32 @@
 extern "C" {
 #endif
 
-typedef uint32_t event_id_t;
+typedef enum {
+    EVENT_ID_NONE           = 0,
+    EVENT_ID_SAFE_OFF       = 1,
+    EVENT_ID_EMERGENCY      = 2,
+    EVENT_ID_DEGRADED       = 3,
+    EVENT_ID_NORMAL         = 4,
+    EVENT_ID_ALERT_RAISED   = 5,
+    EVENT_ID_ALERT_ACKED    = 6,
+    EVENT_ID_PLUG_TOGGLED   = 7,
+    EVENT_ID_FEED_START     = 8,
+    EVENT_ID_FEED_END       = 9,
+    EVENT_ID_CONFIG_CHANGED = 10,
+    EVENT_ID_WIZARD_DONE    = 11,
+    EVENT_ID_RESET_START    = 12,
+    EVENT_ID_TEMP_CRITICAL  = 13,
+    EVENT_ID_OVER_CURRENT   = 14,
+    EVENT_ID_ATO_OVERFLOW   = 15,
+    EVENT_ID_SD_MOUNTED     = 16,
+    EVENT_ID_SD_UNMOUNTED   = 17,
+    EVENT_ID_WDT_TIMEOUT    = 18,
+    EVENT_ID_BOOT_COMPLETE  = 19,
+    EVENT_ID_COUNT
+} event_id_t;
 
-#define EVENT_ID_NONE           0
-#define EVENT_ID_SAFE_OFF       1
-#define EVENT_ID_EMERGENCY      2
-#define EVENT_ID_DEGRADED       3
-#define EVENT_ID_NORMAL         4
-#define EVENT_ID_ALERT_RAISED   5
-#define EVENT_ID_ALERT_ACKED    6
-#define EVENT_ID_PLUG_TOGGLED   7
-#define EVENT_ID_FEED_START     8
-#define EVENT_ID_FEED_END       9
-#define EVENT_ID_CONFIG_CHANGED 10
-#define EVENT_ID_WIZARD_DONE    11
-#define EVENT_ID_RESET_START    12
-#define EVENT_ID_TEMP_CRITICAL  13
-#define EVENT_ID_OVER_CURRENT   14
-#define EVENT_ID_ATO_OVERFLOW   15
-#define EVENT_ID_SD_MOUNTED     16
-#define EVENT_ID_SD_UNMOUNTED   17
-#define EVENT_ID_WDT_TIMEOUT    18
-#define EVENT_ID_BOOT_COMPLETE  19
+#define EVENT_BUS_MAX_SUBSCRIBERS  8
+#define EVENT_BUS_MAX_PENDING     16
 
 typedef void (*event_callback_t)(event_id_t evt, void *data, void *user_ctx);
 
